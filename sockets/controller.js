5c1deb5c0e1c238f9601aebaa2ba874120eb6c93
Santiago <saforero88@gmail.com>
2022-05-18 15:00:17 -0500

    borrado de voxeles y salas

--- sockets/controller.js
+++ sockets/controller.js
@@ -11,10 +11,10 @@ const socketController = async (socket = new Socket(), io) => {
       " usuarios conectados"
   );
   console.log("cliente conectado", socket.id);
-  clients[socket.id] = {
-    position: [0, 0, 0],
-    rotation: [0, 0, 0],
-  };
+  // clients[socket.id] = {
+  //   username: "",
+  //   room: "",
+  // };
   // console.log(clients);
 
   socket.emit(
@@ -34,7 +34,7 @@ const socketController = async (socket = new Socket(), io) => {
   socket.on("move", (pos) => {
     // clients[socket.id].position = pos;
     console.log(pos);
-    io.sockets.emit(
+    io.to(clients[socket.id].room).emit(
       "userPositionsVoxels",
       io.engine.clientsCount,
       socket.id,
@@ -43,6 +43,31 @@ const socketController = async (socket = new Socket(), io) => {
     );
     console.log("puso un voxel", socket.id);
   });
+
+  socket.on("removeVoxel", (pos) => {
+    // clients[socket.id].position = pos;
+    console.log(pos);
+    io.to(clients[socket.id].room).emit(
+      "removeVoxelScene",
+      io.engine.clientsCount,
+      socket.id,
+      Object.keys(clients),
+      pos
+    );
+    console.log("puso un voxel", socket.id);
+  });
+  socket.on("actualizarEscena", (arreglo) => {
+    // clients[socket.id].position = pos;
+    console.log("llego alguien nuevo el arreglo a rederizar es:", arreglo);
+    // io.to(clients[socket.id].room).emit(
+    //   "removeVoxelScene",
+    //   io.engine.clientsCount,
+    //   socket.id,
+    //   Object.keys(clients),
+    //   pos
+    // );
+  });
+
   socket.on("disconnect", () => {
     delete clients[socket.id];
     io.sockets.emit(
@@ -60,6 +85,45 @@ const socketController = async (socket = new Socket(), io) => {
     );
 
     console.log("CLiente Desconectado", socket.id);
+  });
+
+  socket.on("joinRoom", function (req, callback) {
+    console.log(req);
+    if (
+      req.room.replace(/\s/g, "").length > 0 &&
+      req.username.replace(/\s/g, "").length > 0
+    ) {
+      var nameTaken = false;
+
+      console.log("userInfo", clients);
+      Object.keys(clients).forEach((socketId) => {
+        var userInfo = clients[socketId];
+        console.log("req", req.username);
+        if (userInfo.username.toUpperCase() === req.username.toUpperCase()) {
+          nameTaken = true;
+        }
+      });
+
+      if (nameTaken) {
+        callback({
+          nameAvailable: false,
+          error: "Sorry this username is taken!",
+        });
+      } else {
+        clients[socket.id] = req;
+        socket.join(req.room);
+        io.to(clients[socket.id].room).emit("actualizar", socket.id);
+
+        callback({
+          nameAvailable: true,
+        });
+      }
+    } else {
+      callback({
+        nameAvailable: false,
+        error: "Hey, please fill out the form!",
+      });
+    }
   });
 };
 
